879502956f16ad552117ed30298b7a10a0aa8af7
vicentin177 <vicentevidal@liceovvh.cl>
2024-09-05 23:18:35 -0400

    ok

---
+++ index.js
@@ -0,0 +1,6 @@
+function ejemplo(elemento) {
+    console.log("elemento clickeado", elemento);
+    elemento.style.backgroundColor = 'red'; // Cambia el color de fondo del botón
+    elemento.textContent = 'sangrando!'; // Cambia el texto del botón
+}
+
